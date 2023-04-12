a1ae9b9feb7ebd20a7157109893921ac7a0ab4f2
Burak Ocak <ocakbura@gmail.com>
2023-04-13 01:10:43 +0300

    feat: Tours projesi güncellendi.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
+import { useState } from 'react';
 import './App.css';
 import Tours from './Tours';
-
+import toursList from './data';
 
 function App() {
 
+  const [data, setData] = useState(toursList);
 
-
+  function deleteElement(id){
+    const newElement = data.filter( (e)=>e.id !== id );
+    setData(newElement);
+  };
+  
+  if(data.length === 0){
+    return <div>Geriye tur kalmamıştır....</div>
+  }
   return (
-    <div>
+    <div className='mainClass'>
       <h2>Our Tours</h2>
       <div className='underLine' ></div>
 
-      <Tours />
+      {
+        data.map( (e)=> <Tours data={e} del={deleteElement}/> )
+      }
+      
     </div>
   );
 }
--- src/Tours.jsx
+++ src/Tours.jsx
@@ -1,17 +1,17 @@
 import './tours.css'
-function Tours() {
+function Tours({data, del}) {
    
    return(
    <div className='tours' >
-      <div className="single-tour">
-         <img src="" alt="" />
+      <div className="single-tour" style={{border:"1px solid black"}}>
+         <img src={data.image} alt="" />
          <div className="tour-info">
-            <span className="tour-price">19.90</span>
-            <h4>başlık</h4>
-            <p>bilgi
+            <span className="tour-price" style={{color:"white"}}>${data.price}</span>
+            <h4>{data.name}</h4>
+            <p>{data.info}
             <button className='readMore'>Read More</button>
             </p>
-            <button className="delete-btn">not interested</button>
+            <button className="delete-btn" onClick={()=>del(data.id)}>not interested</button>
          </div>
       </div>
    </div>
